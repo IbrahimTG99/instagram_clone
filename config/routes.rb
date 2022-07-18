789a637c78f3fc43df05d319af00b97ccbbf290a
IbrahimTariqDev <ibrahim.tariq@devsinc.com>
2022-07-18 20:02:19 +0500

    added post creation

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # dashboard
+  get '/dashboard' => 'users#index'
+  root to: 'public#homepage'
+  resources :posts, only: %i[new create show]
 end
